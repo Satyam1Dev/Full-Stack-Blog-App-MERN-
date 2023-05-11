dd170b0f64a271b6fc234cbfca20263bc92524de
satyaprakash singh <satyamprakash448Qgmail.com>
2023-05-11 14:52:54 +0530

    back-end

--- backend/api/model/User.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const {Schema,model} = mongoose;
-
-const userSchema = new Schema({
-    username:{type:String, required:true, min:4,unique:true},
-    password:{type:String, required:true},
-});
-const UserModel = model('User',userSchema);
-module.exports = UserModel;
